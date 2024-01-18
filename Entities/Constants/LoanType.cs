760752b3b46a0f1b9d2a39105ed969c9e6816442
iliagognadze <paolokolau@gmail.com>
2024-01-18 17:10:13 +0400

    added basics

---
+++ Entities/Constants/LoanType.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Constants;
+
+public static class LoanType
+{
+    public const string FastLoan = "fast-loan";
+    public const string AutoLoan = "auto-loan";
+    public const string Installment = "installment";
+}
